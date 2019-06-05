9ca3313b5502d8c02320c70d6b3c8df8221b5c80
squishy123 <christian.gnaw@gmail.com>
2019-06-05 15:44:21 -0400

    Added localstorage saving for settings

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -4,23 +4,39 @@ import { Link } from 'gatsby';
 
 import { connect } from 'react-redux';
 
-import { changeBG } from '../../state/actions';
+import { changeReaderBG, changeReaderColor, changeReaderFont } from '../../state/actions';
 
 import styles from './settings.module.scss';
 
 const mapStateToProps = state => {
-    return { bgColor: state.bgColor };
+    return {
+        readerFont: state.readerFont,
+        readerColor: state.readerColor,
+        readerBgColor: state.readerBgColor
+    };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        changeBG: bgColor => dispatch(changeBG(bgColor)),
+        changeReaderBG: readerBgColor => dispatch(changeReaderBG(readerBgColor)),
+        changeReaderColor: readerColor => dispatch(changeReaderColor(readerColor)),
+        changeReaderFont: readerFont => dispatch(changeReaderFont(readerFont)),
     };
 };
 
 class Settings extends React.Component {
     constructor(props) {
         super(props);
+
+        console.log(props);
+
+        this.state = {
+            readerFont: props.readerFont,
+            // lineSpacing: props.lineSpacing,
+            // characterSpacing: props.characterSpacing,
+            readerColor: props.readerColor,
+            readerBgColor: props.readerBgColor
+        }
     }
 
     render() {
@@ -29,9 +45,9 @@ class Settings extends React.Component {
                 <div className={styles.inputGroup}>
                     <div className={styles.inputItem}>
                         <label>Font Family</label>
-                        <select name="font-family">
+                        <select name="font-family" onChange={(e) => { this.props.changeReaderFont(e.target.value) }}>
                             {
-                                ['OpenDyslexic', 'Comic Sans MS', 'Roboto', 'Times New Roman'].map((val, i) =>
+                                ['Arial', 'Open Dyslexic', 'Comic Sans MS', 'Roboto', 'Times New Roman'].map((val, i) =>
                                     <option value={val}>{val}</option>
                                 )
                             }
@@ -49,17 +65,24 @@ class Settings extends React.Component {
                         <label>Font Color</label>
                         <div className={styles.row}>
                             {['yellow', 'white', 'black', 'cyan'].map((val, i) =>
-                                <div className={styles.tile} style={{ backgroundColor: val }} />)}
+                                <div key={val} className={styles.tile} style={{ backgroundColor: val }} onClick={() => { this.props.changeReaderColor(val) }} />)}
                         </div>
                     </div>
                     <div className={styles.inputItem}>
                         <label>Background Color</label>
                         <div className={styles.row}>
                             {['yellow', 'white', 'black', 'cyan'].map((val, i) =>
-                                <div className={styles.tile} style={{ backgroundColor: val }} />)}
+                                <div key={val} className={styles.tile} style={{ backgroundColor: val }} onClick={() => { this.props.changeReaderBG(val) }} />)}
                         </div>
                     </div>
                 </div>
+
+                <h1 style={{
+                    margin: '30px auto',
+                    transition: '.25s linear',
+                    backgroundColor: this.props.readerBgColor,
+                    color: this.props.readerColor, fontFamily: this.props.readerFont
+                }}>This is what Text looks like</h1>
             </div>
         )
     }
